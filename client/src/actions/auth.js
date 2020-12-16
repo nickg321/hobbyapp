4012905b5b6401fa1f35b6052cc034437b57f9bd
Nicholas <nickgreenva@hotmail.com>
2020-12-16 10:47:06 -0600

    Fixed proxy & login/register bugs

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -39,6 +39,10 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     },
   };
 
+  console.log(name);
+  console.log(email);
+  console.log(password);
+
   const body = JSON.stringify({ name, email, password });
 
   try {
@@ -55,6 +59,38 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 
     dispatch({
       type: REGISTER_FAIL,
+    });
+  }
+};
+
+// Login User
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+  } catch (e) {
+    const errors = e.response.data.errors;
+
+    if (errors) {
+      // errors.forEach(error => dispatch())
+      console.log('Login error');
+    }
+    dispatch({
+      type: LOGIN_FAIL,
     });
   }
 };
